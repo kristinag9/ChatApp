40a1e85fd85469a3cdffd08245f2a45185d945ae
kristinag9 <krisitsekova9@gmail.com>
2022-02-03 00:52:30 +0200

    Started adding functionality for adding room

--- Chat-App-Project/src/app.js
+++ Chat-App-Project/src/app.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
-// import { getAnalytics } from "firebase/analytics";
+import { getDatabase, set, push, ref } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,6 +9,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAFVd6h59xX3gOeP4O5LDJQ87w-hJdep64",
   authDomain: "chatapp-57119.firebaseapp.com",
+  // databaseURL: "https://chatapp-57119-default-rtdb.firebaseio.com",
   projectId: "chatapp-57119",
   storageBucket: "chatapp-57119.appspot.com",
   messagingSenderId: "283809287187",
@@ -19,41 +20,89 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-console.log(app);
+const db = getDatabase(app);
+// console.log(db);
 
-// Validate registration
-const form = document.querySelector(".register-form");
-const userName = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const confirmedPassword = document.getElementById("confirm-pass");
+// const dataRef = ref(db, "messages");
+// set(dataRef, {
+//   "a": "b"
+// })
 
-const submitBtn = document.getElementById("submit");
-submitBtn.addEventListener(("click"), (event) => {
-  // userName.value.trim();
-  // email.value.trim();
-  // password.value.trim();
-  // confirmedPassword.value.trim();
+// const messageList;
+// For messages
+// .addEventListener("change". event => {
+//   const dataRef = ref(db, "messages");
+//   const messageRef = push(dataRef);
 
-  if(userName.value === '' ||  email.value === '' || password.value === '' || confirmedPassword.value === '') {
-    // Swal.fire("Please, fill all fields!");
-    alert("Please, fill in all fields!");
-    return;
-  }
+//   Set(messageRef, {
+//     "username": // from db
+//     "date": Date.now().toLocaleString(),
+//     "text": event.target.value
+//   });
 
-  // if (!email.value.matches("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")) {
-  //   alert("Invalid email!");
-  //   return;
-  // }
+// event.target.value = "";
+// })
 
-  if (password.value.length < 6 || !password.value.includes('.') && !password.value.includes('!')) {
-    alert('Weak password');
-    return;
-  }
+// za da vzemem message
+// .addEventListener("change", event => {
+// OT HTML-A VZIMAM FORMATA ZA MESSAGE
+// const message = data.val();
+// const listItem = document.createElement('li');
+// listItem.innerHTML = `
+//   <div class="user-info">
+//     <span class="username><b>${message.username}</b></span>
+//     <span class="date>${message.date}</span>
+//   </div>
+//   <span class="message">${message.text}</span>
+// `
+// messageList.appendChild(listItem);
+// });
 
-  if (password.value != confirmedPassword.value) {
-    alert('Different passwords');
-    return;
-  }
+// Method for opening the dialog box
+const createChatRoomBtn = document.getElementById("create-room-btn");
+const wrapperChatRooms = document.querySelector(".wrapper");
+const modalBackground = document.getElementById("modal-background");
+const createRoomDialog = document.querySelector(".wrapper-modal");
 
+createChatRoomBtn.addEventListener("click", (event) => {
+  modalBackground.classList.toggle("hidden-overflow", open);
+  modalBackground.toggleAttribute("hidden", !open);
+  createRoomDialog.toggleAttribute("hidden", !open);
+  wrapperChatRooms.innerHTML = "";
+  // window.location = "../html/createChatRoomModal.html";
 });
+
+// Adding functionality to the modal form for creating new chat room
+const newRoomForm = document.querySelector(".modal-form");
+const createRoomBtn = document.getElementById("create-btn");
+newRoomForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const newRoomTitle = document.getElementById("title-input").value;
+  if(newRoomTitle === ''){
+    alert("Please, add a title to you new chat room!");
+    return;
+  }
+  const selectedUsers = document.getElementById("adding-section").value;
+  const dataRef = ref(db, 'chat-rooms');
+      const roomsRef = push(dataRef);
+      set(roomsRef, {
+        title: newRoomTitle,
+        members: selectedUsers
+    });
+
+    createRoomDialog.innerHTML = "";
+    modalBackground.classList.toggle("hidden-overflow", !open);
+    modalBackground.toggleAttribute("hidden", open);
+    wrapperChatRooms.toggleAttribute("hidden", !open);
+
+    // const chatRoomList = document.querySelector(".rooms-wrapper");
+    // console.log(chatRoomList);
+    // const listItem = document.createElement('li');
+    // listItem.innerHTML = `
+    //   <button class="room-details">
+    //      <a href="chatRoom.html">${newRoomTitle}</a>
+    //   </button>
+    // `;
+    // chatRoomList.appendChild(listItem);
+})
