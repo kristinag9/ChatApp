e12d2a1403271493f71feed8e4f627d587372533
kristinag9 <krisitsekova9@gmail.com>
2022-02-05 10:31:32 +0200

    Added functionality to create new room

--- Chat-App-Project/src/app.js
+++ Chat-App-Project/src/app.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
-import { getDatabase, set, push, ref } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-database.js";
+import { getDatabase, set, push, ref, onValue } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -58,51 +58,146 @@ const db = getDatabase(app);
 // messageList.appendChild(listItem);
 // });
 
-// Method for opening the dialog box
-const createChatRoomBtn = document.getElementById("create-room-btn");
-const wrapperChatRooms = document.querySelector(".wrapper");
-const modalBackground = document.getElementById("modal-background");
-const createRoomDialog = document.querySelector(".wrapper-modal");
+function getChatRoomsTitles() {
+  const dataRef = ref(db, 'chat-rooms');
+  const roomsTitles = [];
+  onValue(dataRef, (snapshot) => {
+   snapshot.forEach((childSnapshot) => {
+     const childKey = childSnapshot.key;
+     const childData = childSnapshot.val();
+     roomsTitles.push(childData.title);
+   });
+ }, {
+   onlyOnce: true
+ });
+ return roomsTitles;
+};
 
-createChatRoomBtn.addEventListener("click", (event) => {
-  modalBackground.classList.toggle("hidden-overflow", open);
-  modalBackground.toggleAttribute("hidden", !open);
+// Finding all emails of the users
+const findUsersEmails = () => {
+  const dataRef = ref(db, 'users');
+  let emails = [];
+  onValue(dataRef, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const childKey = childSnapshot.key;
+      const childData = childSnapshot.val();
+      emails.push(childData.email);
+    });
+  });
+  return emails;
+};
+
+// Method for opening the dialog box
+const toggleModal = (open) => {
+  let backgroundWrapper = document.querySelector(".wrapper");
+  const createRoomDialog = document.querySelector(".wrapper-modal");
+  backgroundWrapper.classList.toggle("hidden-overflow", open);
+  backgroundWrapper.toggleAttribute("hidden", !open);
   createRoomDialog.toggleAttribute("hidden", !open);
-  wrapperChatRooms.innerHTML = "";
-  // window.location = "../html/createChatRoomModal.html";
-});
+};
+
+const createRoomListElement = (name) => {
+  // const allChatRoomsTitles = getChatRoomsTitles();
+  const chatRoomList = document.querySelector(".rooms-wrapper");
+  const listItem = document.createElement('li');
+  // allChatRoomsTitles.map(title => {
+    listItem.innerHTML = `
+    <button class="room-details">
+      <a href="chatRoom.html">${name}</a>
+    </button>`;
+    chatRoomList.appendChild(listItem);
+  // });
+}
+
+// const listMembers = document.querySelector(".show-users-emails");
+const membersList = document.querySelector(".show-users-emails");
+const memberListTemplate = (email) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("user");
+
+  const emailTag = document.createElement("h4");
+  emailTag.classList.add("user-email");
+  emailTag.innerText = email;
+
+  const addButton = document.createElement("button");
+  addButton.classList.add("add-member-btn");
+  addButton.innerText = "+";
+
+  listItem.appendChild(emailTag);
+  listItem.appendChild(addButton);
+
+  membersList.appendChild(listItem);
+}
+
+// console.log(usersEmails);
+// const loadUsersEmails = async() => {
+//   const usersEmails = findUsersEmails();
+//   console.log(usersEmails);
+//   usersEmails.map((email) => memberListTemplate(email));
+// };
+
 
 // Adding functionality to the modal form for creating new chat room
-const newRoomForm = document.querySelector(".modal-form");
-const createRoomBtn = document.getElementById("create-btn");
-newRoomForm.addEventListener("submit", event => {
-  event.preventDefault();
+const createNewRoom = () => {
+  const inputTitleRoom = document.getElementById("title-input");
+  inputTitleRoom.addEventListener("change", (event) => {
+    let newRoomTitle = inputTitleRoom.value;
+     if(newRoomTitle === ''){
+        alert("Please, add a title to your new chat room!");
+        // createRoomModalButton.setAttribute("disabled", "");
+        return;
+      }
 
-  const newRoomTitle = document.getElementById("title-input").value;
-  if(newRoomTitle === ''){
-    alert("Please, add a title to you new chat room!");
-    return;
-  }
-  const selectedUsers = document.getElementById("adding-section").value;
-  const dataRef = ref(db, 'chat-rooms');
+      const allRoomsTitles = getChatRoomsTitles();
+      if(allRoomsTitles.some(title => title = newRoomTitle)) {
+        alert("The name is already taken! Choose another one!");
+        return;
+      }
+
+        // const searchingUsers = document.getElementById("adding-section").value;
+
+          // const textArea = document.querySelector(".text-area");
+          // const usersEmails = findUsersEmails();
+          // if(usersEmails.some(email => email !== searchingUsers.value)) {
+          //   alert("No user found!");
+          // }
+          // usersEmails.forEach((email) => memberListTemplate(email));
+          // memberListTemplate(searchingUsers.value);
+          // loadUsers();
+
+      const dataRef = ref(db, 'chat-rooms');
       const roomsRef = push(dataRef);
-      set(roomsRef, {
-        title: newRoomTitle,
-        members: selectedUsers
+        set(roomsRef, {
+          title: newRoomTitle,
+          members: ''
+        });
+      createRoomListElement(newRoomTitle);
+  });
+}
+
+const usersEmails = findUsersEmails();
+
+const createChatRoomBtn = document.getElementById("create-room-btn");
+if(createChatRoomBtn) {
+  createChatRoomBtn.addEventListener("click", (event) => {
+    membersList.innerText = "";
+    usersEmails.map(email => memberListTemplate(email));
+    toggleModal(true);
+  });
+};
+createNewRoom();
+
+const createRoomModalButton = document.getElementById("create-btn-modal");
+if(createRoomModalButton) {
+    createRoomModalButton.addEventListener("click", event => {
+      const inputTitleRoom = document.getElementById("title-input");
+      inputTitleRoom.value = "";
+      toggleModal(false);
+
     });
+}
 
-    createRoomDialog.innerHTML = "";
-    modalBackground.classList.toggle("hidden-overflow", !open);
-    modalBackground.toggleAttribute("hidden", open);
-    wrapperChatRooms.toggleAttribute("hidden", !open);
-
-    // const chatRoomList = document.querySelector(".rooms-wrapper");
-    // console.log(chatRoomList);
-    // const listItem = document.createElement('li');
-    // listItem.innerHTML = `
-    //   <button class="room-details">
-    //      <a href="chatRoom.html">${newRoomTitle}</a>
-    //   </button>
-    // `;
-    // chatRoomList.appendChild(listItem);
-})
+const closeModalBtn = document.getElementById("close-modal-btn");
+closeModalBtn.addEventListener("click", event => {
+    toggleModal(false);
+});
