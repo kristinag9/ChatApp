229dbe2e61d0c1a7dd37448e7d75634b14a6ca80
kristinag9 <krisitsekova9@gmail.com>
2022-02-06 11:29:32 +0200

    Added functionality to add members to room

--- Chat-App-Project/src/app.js
+++ Chat-App-Project/src/app.js
@@ -58,7 +58,7 @@ const db = getDatabase(app);
 // messageList.appendChild(listItem);
 // });
 
-function getChatRoomsTitles() {
+const getChatRoomsTitles = () => {
   const dataRef = ref(db, 'chat-rooms');
   const roomsTitles = [];
   onValue(dataRef, (snapshot) => {
@@ -72,6 +72,25 @@ function getChatRoomsTitles() {
  });
  return roomsTitles;
 };
+
+const getEmailsToAdd = () => {
+  const dataRef = ref(db, 'added-members');
+  const emailsToAdd = [];
+  onValue(dataRef, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const childKey = childSnapshot.key;
+      const childData = childSnapshot.val();
+      emailsToAdd.push(childData.emailToAdd);
+      // dataRef.childKey('emailToAdd').set(null);
+    });
+  }, {
+    onlyOnce: true
+  });
+  return emailsToAdd;
+}
+
+// const allemails = getEmailsToAdd();
+// console.log(allemails);
 
 // Finding all emails of the users
 const findUsersEmails = () => {
@@ -109,50 +128,90 @@ const createRoomListElement = (name) => {
   btnRoom.innerText = `${name}`;
   listItem.appendChild(btnRoom);
   chatRoomList.appendChild(listItem);
-  // btnRoom.addEventListener("click", event => {
-  //   window.location = "/html/chatRoom.html";
-  //   const headerChatRoomName = document.getElementById("chat-room-name");
-  //   console.log(headerChatRoomName);
-  //   headerChatRoomName.innerText = `${name}`;
-  //   console.log(headerChatRoomName);
-  // });
-  // });
+  btnRoom.addEventListener("click", event => {
+    window.location = "/html/chatRoom.html";
+  });
+}
+
+// Saving the new room in the database
+const createNewRoom = (name, users) => {
+  const dataRef = ref(db, 'chat-rooms');
+  const roomsRef = push(dataRef);
+    set(roomsRef, {
+      title: name,
+      members: users ?? []
+    });
+}
+
+// const appendToStorage = (key, data) => {
+//   var oldData = localStorage.getItem(key);
+//   if(oldData === null) {
+//     oldData = "";
+//   }
+//   localStorage.setItem(key, oldData + ' ' + data);
+// }
+
+// In local storage we cannot append data to one key. So, we can do it by assigning
+// index numbers to the key name.
+// This function shows how to append data to different keys
+const appendToLocalStorage = (key, value) => {
+  let firstFreeIndex = 0;
+  while (localStorage.getItem(key + firstFreeIndex)) {
+    firstFreeIndex++; // if there is an item with this index, go ahead
+  }
+  localStorage.setItem(key + firstFreeIndex, value); //add item at first available index number
+}
+
+// Get all the data (from the different keys) into one array
+const getLocalStorage = (name) => {
+  let firstFreeNumber = 0;
+  let data = [];
+  while (localStorage.getItem(name + firstFreeNumber)) {
+      data.push(localStorage.getItem(name + firstFreeNumber++));
+  }
+  return data;
 }
 
 // For each email of the users emails in the database create a modal which appends
 // each email item to the modal for creating a new room
 const membersList = document.querySelector(".show-users-emails");
-const memberListTemplate = (email) => {
-  const listItem = document.createElement("li");
-  listItem.classList.add("user");
+const membersModal = () => {
+  const usersEmails = findUsersEmails();
+  usersEmails.map(email => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("user");
 
-  const emailTag = document.createElement("h4");
-  emailTag.classList.add("user-email");
-  emailTag.innerText = email;
+    const emailTag = document.createElement("h4");
+    emailTag.classList.add("user-email");
+    emailTag.innerText = email;
 
-  const addButton = document.createElement("button");
-  addButton.classList.add("add-member-btn");
-  addButton.innerText = "+";
+    const addButton = document.createElement("button");
+    addButton.classList.add("add-member-btn");
+    addButton.innerText = "+";
 
-  listItem.appendChild(emailTag);
-  listItem.appendChild(addButton);
-  membersList.appendChild(listItem);
-}
+    listItem.appendChild(emailTag);
+    listItem.appendChild(addButton);
+    membersList.appendChild(listItem);
 
-// Saving the new room in the database
-const createNewRoom = (name) => {
-  const dataRef = ref(db, 'chat-rooms');
-  const roomsRef = push(dataRef);
-    set(roomsRef, {
-      title: name,
-      members: ''
+    const usersToAdd = [];
+    window.localStorage.setItem("emailsToAdd", usersToAdd);
+    addButton.addEventListener("click", event => {
+      event.preventDefault();
+      appendToLocalStorage("emailsToAdd", emailTag.innerText);
+      // if(usersToAdd.find(email => email === emailToAdd)) {
+      //   alert("This user is already added!");
+      //   return;
+      // }
+      membersList.removeChild(listItem);
     });
+  });
 }
+
 
 // Add functionality to the create button of the modal for creating a new room
 const createRoomModalButton = document.getElementById("create-btn-modal");
 if(createRoomModalButton) {
-    createRoomModalButton.addEventListener("click", event => {
+  createRoomModalButton.addEventListener("click", event => {
       event.preventDefault();
       const inputTitleRoom = document.getElementById("title-input");
       let inputValue = inputTitleRoom.value.trim();
@@ -174,7 +233,15 @@ if(createRoomModalButton) {
         alert("The name is already taken! Choose another one!");
         return;
       }
-      createNewRoom(inputValue);
+
+      window.localStorage.setItem("roomName", inputValue);
+
+      // Get the array with selected emails from the local storage
+      const emailsToAdd = getLocalStorage("emailsToAdd");
+      console.log(emailsToAdd);
+
+      createNewRoom(inputValue, emailsToAdd);
+      // membersModal();
       createRoomListElement(inputValue);
       toggleModal(false);
       inputTitleRoom.value = "";
@@ -183,24 +250,36 @@ if(createRoomModalButton) {
 
 // Add functionality to the close button of the modal for creating a new room
 const closeModalBtn = document.getElementById("close-modal-btn");
-closeModalBtn.addEventListener("click", event => {
-    event.preventDefault();
-    const inputTitleRoom = document.getElementById("title-input");
-    toggleModal(false);
-    inputTitleRoom.value = "";
-});
-
+if(closeModalBtn) {
+  closeModalBtn.addEventListener("click", event => {
+      event.preventDefault();
+      const inputTitleRoom = document.getElementById("title-input");
+      toggleModal(false);
+      inputTitleRoom.value = "";
+  });
+}
 // Add functionality to the create room button -> opening the modal for creating a room
 const createChatRoomBtn = document.getElementById("create-room-btn");
 if(createChatRoomBtn) {
-  const usersEmails = findUsersEmails();
   createChatRoomBtn.addEventListener("click", event => {
     event.preventDefault();
     membersList.innerText = "";
-    usersEmails.map(email => memberListTemplate(email));
+    membersModal();
     toggleModal(true);
+
   });
 };
 
 // FUNCTIONALITY TO CHAT ROOM PAGE
+const headerChatRoomName = document.getElementById("chat-room-name");
+console.log(headerChatRoomName);
+const roomName = window.localStorage.getItem("roomName");
+headerChatRoomName.innerText = `${roomName}`;
 
+const setUserName = document.querySelector(".sender-name");
+const usernameFromLocal = window.localStorage.getItem("username");
+setUserName.innerText = `${usernameFromLocal}`;
+
+const setData = document.querySelector(".date");
+const newDate = new Date();
+setData.innerText = newDate.toLocaleString();
